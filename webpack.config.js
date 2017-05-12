8054cdce5170660460a8ac98161e9ab62ec623b4
James Yu <james_yu@asus.com>
2017-05-12 11:30:48 +0800

    Vendor code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path')
+const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'react-redux',
+  'react-router', 'redux', 'redux-form'
+]
+
 const config = {
-  entry: './client/src/index.js',
+  entry: {
+    bundle: './client/src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
-    path: path.resolve(__dirname, 'client/build'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'client/build'),
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -25,6 +34,9 @@ const config = {
     ]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
     new ExtractTextPlugin('styles.css'),
     new HtmlWebpackPlugin({
       template: './client/src/index.html'
