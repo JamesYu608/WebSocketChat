9314197aa9acae57ddcc1ccb77b1c766a5fac064
James Yu <james_yu@asus.com>
2017-05-15 17:17:46 +0800

    Revert "Server: GetDeviceListBySN"
    This reverts commit 20ceb8a1d2aca2c0734f3d57430c165f577e02a7.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,8 @@
 const router = require('express').Router()
-const ZenUIMsgDB = require('../zuimsgdb')
 
 router.get('/', (req, res, next) => {
   res.send({
     greeting: 'hello, world!'
-  })
-})
-
-router.get('/sn', (req, res, next) => {
-  const sn = req.query.sn
-  if (!sn) return res.status(400).send({error: 'You must provide a serial number (query field: sn)'})
-
-  ZenUIMsgDB.GetDeviceListBySN(sn, (err, result) => {
-    return err ? res.status(422).send({error: 'Unable to process the contained instructions'}) : res.json(result)
   })
 })
 
--- server/test/api_test.js
+++ server/test/api_test.js
@@ -1,42 +1,14 @@
 import React from 'react'
 import { expect } from 'chai'
 import request from 'request'
-import querystring from 'querystring'
-
-const API_URL_PREFIX = 'http://localhost:8050/api'
 
 describe('api', () => {
   it('/', (done) => {
-    request(API_URL_PREFIX, (err, response, body) => {
+    request('http://localhost:8050/api', (err, response, body) => {
       if (err) done(err)
 
       expect(JSON.parse(body).greeting).to.equal('hello, world!')
       done()
-    })
-  })
-
-  describe('/devicelist', () => {
-    it('/sn: No parameter', (done) => {
-      request(`${API_URL_PREFIX}/sn`, (err, response, body) => {
-        if (err) done(err)
-
-        expect(response.statusCode).to.equal(400)
-        done()
-      })
-    })
-
-    it('/sn', (done) => {
-      const query = querystring.stringify({sn: 'F9AZCY35T704'})
-      request(`${API_URL_PREFIX}/sn?${query}`, (err, response, body) => {
-        if (err) done(err)
-
-        expect(response.statusCode).to.equal(200)
-
-        const firstResult = JSON.parse(body)[0]
-        expect(firstResult).to.include.keys('device_id')
-        expect(firstResult).to.include.keys('sn')
-        done()
-      })
     })
   })
 })
